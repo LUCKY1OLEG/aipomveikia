8c66a7a6a572dea5e81beac483da9509a299fb3c
Mantas Venskus <mantas.venskus@kreios.lu>
2017-07-30 22:32:53 +0300

    sound icon toggle added

--- js/Script.js
+++ js/Script.js
@@ -1,1 +1,10 @@
-alert("hi")
+$(function() {
+    $('.sound-on').on('click', function(){
+			console.log('turn up the sound');
+			// inline if statement
+			// kintamasis = salyga ? 'grazina jei true' : 'grazina jei false';
+			var text = $(this).html() === 'OFF' ? 'ON' : 'OFF';
+			$(this).html(text);
+    });
+});
+//alert("hi");
